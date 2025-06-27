6cd13dc58e23ac5f8b4483ad087e44793dfac5f1
Jesse <96941057+JesseRMU@users.noreply.github.com>
2025-06-27 12:34:16 +0200

    vite main.blade configuration gefixed

--- resources/views/components/layout/main.blade.php
+++ resources/views/components/layout/main.blade.php
@@ -7,7 +7,7 @@
     <title>TaskITEasy</title>
 
     {{-- Compiled assets --}}
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     {{-- Render additional styles from subviews and/or components --}}
     @stack('styles')
 </head>
